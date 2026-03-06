7db785ce3784f0606b63797083511a335c2ef5a1
shift-developer-1 <shiftdefi-dev-1@proton.me>
2026-03-06 17:27:40 +0300

    fix: remove fork instance assignment duplicate

--- test/CCTPv2BridgeAdapterEthereum.t.sol
+++ test/CCTPv2BridgeAdapterEthereum.t.sol
@@ -20,20 +20,18 @@ contract CCTPv2BridgeAdapterEthereumTest is CCTPv2BridgeAdapterTest {
         string memory ETHEREUM_RPC = vm.envString("ETH_RPC_URL");
         string memory ARBITRUM_RPC = vm.envString("ARB_RPC_URL");
 
-        Fork memory l1Fork = Fork({
+        _setUp(Fork({
             rpc: ETHEREUM_RPC,
             tokenMessengerV2: ETHEREUM_TOKEN_MESSENGER_V2,
             usdc: ETHEREUM_USDC,
             chainId: ETHEREUM_CHAIN_ID,
             domainId: ETHEREUM_DOMAIN_ID
-        });
-        Fork memory l2Fork = Fork({
+        }), Fork({
             rpc: ARBITRUM_RPC,
             tokenMessengerV2: ARBITRUM_TOKEN_MESSENGER_V2,
             usdc: ARBITRUM_USDC,
             chainId: ARBITRUM_CHAIN_ID,
             domainId: ARBITRUM_DOMAIN_ID
-        });
-        _setUp(l1Fork, l2Fork);
+        }));
     }
 }
